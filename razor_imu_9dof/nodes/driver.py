0f4c589721960b5ac0e0d5f462d924f7c891bf57
robotictang <tang.tiong.yew@gmail.com>
2013-01-03 09:37:41 +0800

    Initial commit

---
+++ razor_imu_9dof/nodes/driver.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2012, Tang Tiong Yew
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of the Willow Garage, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib; roslib.load_manifest('razor_imu_9dof')
+import rospy
+
+from visual import *
+import serial
+import string
+import math
+
+from time import time
+from sensor_msgs.msg import Imu
+import tf
+
+grad2rad = 3.141592/180.0
+
+rospy.init_node("driver")
+pub = rospy.Publisher('imu', Imu)
+imuMsg = Imu()
+imuMsg.orientation_covariance = [999999 , 0 , 0,
+0, 9999999, 0,
+0, 0, 999999]
+imuMsg.angular_velocity_covariance = [9999, 0 , 0,
+0 , 99999, 0,
+0 , 0 , 0.02]
+imuMsg.linear_acceleration_covariance = [0.2 , 0 , 0,
+0 , 0.2, 0,
+0 , 0 , 0.2]
+
+
+# Check your COM port and baud rate
+ser = serial.Serial(port='/dev/ttyUSB1',baudrate=57600, timeout=1)
+
+
+
+
+# Main scene
+scene=display(title="9DOF Razor IMU Main Screen")
+scene.range=(1.2,1.2,1.2)
+#scene.forward = (0,-1,-0.25)
+scene.forward = (1,0,-0.25)
+scene.up=(0,0,1)
+
+# Second scene (Roll, Pitch, Yaw)
+scene2 = display(title='Second Screen',x=0, y=0, width=500, height=200,center=(0,0,0), background=(0,0,0))
+scene2.range=(1,1,1)
+scene.width=500
+scene.height=500
+scene.y=200
+
+scene2.select()
+#Roll, Pitch, Yaw
+cil_roll = cylinder(pos=(-0.4,0,0),axis=(0.2,0,0),radius=0.01,color=color.red)
+cil_roll2 = cylinder(pos=(-0.4,0,0),axis=(-0.2,0,0),radius=0.01,color=color.red)
+cil_pitch = cylinder(pos=(0.1,0,0),axis=(0.2,0,0),radius=0.01,color=color.green)
+cil_pitch2 = cylinder(pos=(0.1,0,0),axis=(-0.2,0,0),radius=0.01,color=color.green)
+#cil_course = cylinder(pos=(0.6,0,0),axis=(0.2,0,0),radius=0.01,color=color.blue)
+#cil_course2 = cylinder(pos=(0.6,0,0),axis=(-0.2,0,0),radius=0.01,color=color.blue)
+arrow_course = arrow(pos=(0.6,0,0),color=color.cyan,axis=(-0.2,0,0), shaftwidth=0.02, fixedwidth=1)
+
+#Roll,Pitch,Yaw labels
+label(pos=(-0.4,0.3,0),text="Roll",box=0,opacity=0)
+label(pos=(0.1,0.3,0),text="Pitch",box=0,opacity=0)
+label(pos=(0.55,0.3,0),text="Yaw",box=0,opacity=0)
+label(pos=(0.6,0.22,0),text="N",box=0,opacity=0,color=color.yellow)
+label(pos=(0.6,-0.22,0),text="S",box=0,opacity=0,color=color.yellow)
+label(pos=(0.38,0,0),text="W",box=0,opacity=0,color=color.yellow)
+label(pos=(0.82,0,0),text="E",box=0,opacity=0,color=color.yellow)
+label(pos=(0.75,0.15,0),height=7,text="NE",box=0,color=color.yellow)
+label(pos=(0.45,0.15,0),height=7,text="NW",box=0,color=color.yellow)
+label(pos=(0.75,-0.15,0),height=7,text="SE",box=0,color=color.yellow)
+label(pos=(0.45,-0.15,0),height=7,text="SW",box=0,color=color.yellow)
+
+L1 = label(pos=(-0.4,0.22,0),text="-",box=0,opacity=0)
+L2 = label(pos=(0.1,0.22,0),text="-",box=0,opacity=0)
+L3 = label(pos=(0.7,0.3,0),text="-",box=0,opacity=0)
+
+# Main scene objects
+scene.select()
+# Reference axis (x,y,z)
+arrow(color=color.green,axis=(1,0,0), shaftwidth=0.02, fixedwidth=1)
+arrow(color=color.green,axis=(0,-1,0), shaftwidth=0.02 , fixedwidth=1)
+arrow(color=color.green,axis=(0,0,-1), shaftwidth=0.02, fixedwidth=1)
+# labels
+label(pos=(0,0,0.8),text="9DOF Razor IMU test",box=0,opacity=0)
+label(pos=(1,0,0),text="X",box=0,opacity=0)
+label(pos=(0,-1,0),text="Y",box=0,opacity=0)
+label(pos=(0,0,-1),text="Z",box=0,opacity=0)
+# IMU object
+platform = box(length=1, height=0.05, width=1, color=color.red)
+p_line = box(length=1,height=0.08,width=0.1,color=color.yellow)
+plat_arrow = arrow(color=color.cyan,axis=(1,0,0), shaftwidth=0.06, fixedwidth=1)
+
+
+#f = open("Serial"+str(time())+".txt", 'w')
+
+roll=0
+pitch=0
+yaw=0
+ser.write('#ot' + chr(13))
+while 1:
+    line = ser.readline()
+    line = line.replace("#YPR=","")   # Delete "#YPR="
+    print line
+    #f.write(line)                     # Write to the output log file
+    words = string.split(line,",")    # Fields split
+    if len(words) > 2:
+        try:
+            yaw = float(words[0])*grad2rad
+            pitch = -float(words[1])*grad2rad
+            roll = -float(words[2])*grad2rad
+        except:
+            print "Invalid line"
+
+        axis=(cos(pitch)*cos(yaw),-cos(pitch)*sin(yaw),sin(pitch)) 
+        up=(sin(roll)*sin(yaw)+cos(roll)*sin(pitch)*cos(yaw),sin(roll)*cos(yaw)-cos(roll)*sin(pitch)*sin(yaw),-cos(roll)*cos(pitch))
+        platform.axis=axis
+        platform.up=up
+        platform.length=1.0
+        platform.width=0.65
+        plat_arrow.axis=axis
+        plat_arrow.up=up
+        plat_arrow.length=-0.8
+        p_line.axis=axis
+        p_line.up=up
+        cil_roll.axis=(0.2*cos(roll),0.2*sin(roll),0)
+        cil_roll2.axis=(-0.2*cos(roll),-0.2*sin(roll),0)
+        cil_pitch.axis=(0.2*cos(pitch),0.2*sin(pitch),0)
+        cil_pitch2.axis=(-0.2*cos(pitch),-0.2*sin(pitch),0)
+        arrow_course.axis=(0.2*sin(yaw),0.2*cos(yaw),0)
+        L1.text = words[0]
+        L2.text = words[1]
+        L3.text = words[2]  
+        
+        # Publish message
+        #    imuMsg.angular_velocity.x = rawMsg.angular_velocity.x
+        #    imuMsg.angular_velocity.y = rawMsg.angular_velocity.y
+        #    imuMsg.angular_velocity.z = rawMsg.angular_velocity.z
+            
+        #    imuMsg.linear_acceleration.x = rawMsg.linear_acceleration.x
+        #    imuMsg.linear_acceleration.y = rawMsg.linear_acceleration.y
+        #    imuMsg.linear_acceleration.z = rawMsg.linear_acceleration.z
+            
+        q = tf.transformations.quaternion_from_euler(roll,pitch,yaw)
+        imuMsg.orientation.x = q[0]
+        imuMsg.orientation.y = q[1]
+        imuMsg.orientation.z = q[2]
+        imuMsg.orientation.w = q[3]
+        imuMsg.header.stamp= rospy.Time.now()
+        imuMsg.header.frame_id = 'base_link'
+            
+        pub.publish(imuMsg)
+ser.close
+#f.close
